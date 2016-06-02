3095ca78875b20b6b5f9bee3277c531c3d187802
Leva <lizardesan@gmail.com>
2016-06-02 20:27:21 +0300

    almost done :D

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
 	// tooltips init
 	$(function () {
 	  $('[data-toggle="tooltip"]').tooltip()
-	})
+	});
 
+
+	// mobile menu toggle
+	$('[data-toggle="mobile-menu"]').click(function() {
+		$('.menu-container').slideToggle();
+	});
+
+	// search form toggle
+	$('[data-toggle="search-box"]').click(function() {
+		$('.search input').fadeToggle(50);
+	});
+
+	// accordion
 });
